1c03f01a0d8d40c9f47e8660f0e289a435cfab6f
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-04-17 04:46:36 +0300

    Added xlsx to json exporting script

---
+++ Tool/exporter_script.py
@@ -0,0 +1,60 @@
+import openpyxl
+import json
+from datetime import datetime
+from urllib.parse import urlparse
+
+# Open the Excel file
+workbook = openpyxl.load_workbook('ExporterSheet.xlsx')
+worksheet = workbook.active
+
+# Create a dictionary to store the data
+data = {}
+
+# Load existing data from the output.json file if it exists
+try:
+    with open("Compromised-Discord-Accounts.json", "r") as file:
+        data = json.load(file)
+except FileNotFoundError:
+    pass
+
+# Iterate over the rows in the worksheet
+for row_number, row in enumerate(worksheet.iter_rows(min_row=2, values_only=True), start=1):
+    # Extract the values from the row
+    NOUMBER, FOUND_ON, DISCORD_ID, USERNAME, BEHAVIOUR, TYPE, METHOD, TARGET, PLATFORM, SURFACE_URL, REGION, STATUS = row
+
+    # Convert the found_on datetime object to a string
+    found_on_str = FOUND_ON.strftime('%Y-%m-%d')
+
+    # Get the domain from the SURFACE_URL
+    parsed_url = urlparse(SURFACE_URL)
+    surface_url_domain = parsed_url.netloc
+
+    # Create a dictionary for the current account
+    account = {
+        "CASE_NUMBER": f"{row_number}",
+        "FOUND_ON": found_on_str,
+        "DISCORD_ID": str(DISCORD_ID),
+        "USERNAME": USERNAME,
+        "BEHAVIOUR": BEHAVIOUR,
+        "ATTACK_METHOD": TYPE,
+        "ATTACK_VECTOR": METHOD,
+        "ATTACK_GOAL": TARGET,
+        "ATTACK_SURFACE": PLATFORM,
+        "SUSPECTED_REGION_OF_ORIGIN": REGION,
+        "SURFACE_URL": SURFACE_URL,
+        "SURFACE_URL_DOMAIN": surface_url_domain,
+        "SURFACE_URL_STATUS": STATUS,
+        "FINAL_URL": "",
+        "FINAL_URL_DOMAIN": "",
+        "FINAL_URL_STATUS": ""
+    }
+
+    # Update the data dictionary with the new account
+    data[f"ACCOUNT_NUMBER_{row_number}"] = account
+
+# Convert the data dictionary to JSON
+json_data = json.dumps(data, indent=2)
+
+# Write the JSON data to a file
+with open("Compromised-Discord-Accounts.json", "w") as file:
+    file.write(json_data)
