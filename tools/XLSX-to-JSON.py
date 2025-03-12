d55e8eee04f961df2d19cd990db38ab64a6028d4
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2025-03-12 13:04:03 +0200

    Updated indentation used by tool

--- tools/XLSX-to-JSON.py
+++ tools/XLSX-to-JSON.py
@@ -82,7 +82,7 @@ for row_number, row in enumerate(
     existing_discord_ids.add(str(DISCORD_ID))
 
 # Convert the data dictionary to JSON
-json_data = json.dumps(data, indent=2, ensure_ascii=False)
+json_data = json.dumps(data, indent=4, ensure_ascii=False)
 
 # Write the JSON data to a file
 with open("Compromised-Discord-Accounts.json", "w", encoding='utf-8') as file:
