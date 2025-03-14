e2ded5f8feb6e2a99909f577aa41bae1f60611ba
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2025-03-14 21:19:16 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -6,6 +6,7 @@ const rowsPerPage = 10;
 
 // Existing DOMContentLoaded event listener
 document.addEventListener('DOMContentLoaded', () => {
+    updateBreadcrumbs(); // Call updateBreadcrumbs on DOMContentLoaded
 
     // Add event listener for the info button
     const infoButton = document.getElementById('infoButton');
